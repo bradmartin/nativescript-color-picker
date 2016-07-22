3d25f6f391a96853c7e3dfb30fac9f2b8e79a58e
Sean Perkins <sean@meetmaestro.com>
2016-07-22 04:14:38 -0400

    Adds MSColorPicker for iOS

--- colorpicker.ios.ts
+++ colorpicker.ios.ts
@@ -1,3 +1,32 @@
+var frame = require("ui/frame");
+
+import * as app from 'application';
+
+import {Color} from 'color';
+
+declare var MSColorSelectionViewController, MSColorSelectionViewControllerDelegate, NSObject;
+
 export class ColorPicker {
-  return;
+
+    constructor() { }
+
+    public show(initialColor: string = '#FF4081', colorMode: any = 'RGB') {
+        return new Promise((resolve, reject) => {
+            try {
+                let colorPickerView = new MSColorSelectionViewController();
+                colorPickerView.color = new Color(initialColor).ios;
+                colorPickerView.delegate = NSObject.extend({
+                    colorViewControllerDidChangeColor(colorViewCntroller, color) {
+                        resolve(color);
+                    }
+                }, {
+                    protocols: [MSColorSelectionViewControllerDelegate]
+                }).alloc().init();
+                frame.topmost().currentPage.ios.presentViewControllerAnimatedCompletion(colorPickerView, true, null);
+            } catch(err) {
+                reject(err);
+            }
+        });
+    }
+
 }
