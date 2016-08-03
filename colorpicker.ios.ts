cb3f6797a1b53f14850deb151249be457b1b8c9b
Sean Perkins <sean@meetmaestro.com>
2016-08-02 23:50:57 -0400

    Add iOS declarations, update iOS implementation to allow for closing view

--- colorpicker.ios.ts
+++ colorpicker.ios.ts
@@ -4,29 +4,70 @@ import * as app from 'application';
 
 import {Color} from 'color';
 
-declare var MSColorSelectionViewController, MSColorSelectionViewControllerDelegate, NSObject;
+declare var interop: any,
+            MSColorSelectionViewController: any,
+            MSColorSelectionViewControllerDelegate: any,
+            UINavigationController: any,
+            UIBarButtonItem: any,
+            UIBarButtonItemStyleDone:any;
+
+class ColorPickerImpl extends NSObject {
+
+    private _owner: WeakRef<ColorPicker>;
+
+    public static initWithOwner(owner: WeakRef<ColorPicker>): ColorPickerImpl {
+        let handler = <ColorPickerImpl>ColorPickerImpl.new();
+        handler._owner = owner;
+        return handler;
+    }
+
+    public dismissViewController(sender: any): void {
+        let owner = this._owner.get();
+        if (owner) {
+            owner._dismissViewController(sender);
+        }
+    }
+
+    public static ObjCExposedMethods = {
+        "dismissViewController": { returns: interop.types.void, params: [] }
+    };
+}
 
 export class ColorPicker {
 
-    constructor() { }
+    private impl: ColorPickerImpl;
 
     public show(initialColor: string = '#FF4081', colorMode: any = 'RGB') {
         return new Promise((resolve, reject) => {
             try {
-                let colorPickerView = new MSColorSelectionViewController();
-                colorPickerView.color = new Color(initialColor).ios;
-                colorPickerView.delegate = NSObject.extend({
+                let colorSelectionController = new MSColorSelectionViewController();
+                colorSelectionController.color = new Color(initialColor).ios;
+
+                let navCtrl = UINavigationController.alloc().initWithRootViewController(colorSelectionController);
+
+                let delegate = NSObject.extend({
                     colorViewControllerDidChangeColor(colorViewCntroller, color) {
                         resolve(color);
-                    }
+                    },
                 }, {
                     protocols: [MSColorSelectionViewControllerDelegate]
                 }).alloc().init();
-                frame.topmost().currentPage.ios.presentViewControllerAnimatedCompletion(colorPickerView, true, null);
+                colorSelectionController.delegate = delegate;
+
+                this.impl = ColorPickerImpl.initWithOwner(new WeakRef(this));
+
+                let doneBtn = UIBarButtonItem.alloc().initWithTitleStyleTargetAction('Done', UIBarButtonItemStyleDone, this.impl, 'dismissViewController');
+                colorSelectionController.navigationItem.rightBarButtonItem = doneBtn;
+
+                frame.topmost().currentPage.ios.presentViewControllerAnimatedCompletion(navCtrl, true, null);
             } catch(err) {
                 reject(err);
             }
         });
+    }
+
+    _dismissViewController(sender) {
+        frame.topmost().currentPage.ios.dismissViewControllerAnimatedCompletion(true, null);
     }
 
 }
