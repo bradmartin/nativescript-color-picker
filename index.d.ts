2dffcb56265c7b81bbbee7ed2a7c9688a30274ff
Brad Martin <bradwaynemartin@gmail.com>
2016-08-05 20:26:17 -0500

    bump - ios fix

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,9 @@
 export declare class ColorPicker {
-    constructor();
+
+    /**
+     * Opens the color picker.
+     * @param {string} [initialColor=#ff4081] - The initial color of the picker.
+     * @param {string} [colorMode='RGB'] - The mode of the color picker.
+     */
     show(initialColor?: string, colorMode?: any): Promise<{}>;
-    private _setColorMode(value);
 }
